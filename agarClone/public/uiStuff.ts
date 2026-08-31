5b162c028be7d307a1fb86a981cfc066f1082c6d
max.tsai <max.tsai@twsit.com>
2026-08-31 17:45:59 +0800

    49: Getting the DOM setup

--- agarClone/public/uiStuff.ts
+++ agarClone/public/uiStuff.ts
@@ -3,7 +3,19 @@
 declare const io: any;
 declare const bootstrap: any;
 
-// Shape of the local player. Loose for now - the course fills it in.
+// set heigh and width of canvas = windo
+let wHeight = window.innerHeight;
+let wWidth = window.innerWidth;
+
+// canvas element needs to be in a variable
+const canvas = document.querySelector('#the-canvas') as HTMLCanvasElement;
+
+// context is how we draw, we will be drawing in 2d
+const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+canvas.width = wWidth;
+canvas.height = wHeight;
+
+// shape of the local player - loose for now, the course fills it in
 interface PlayerState {
     name?: string;
     locX?: number;
@@ -13,45 +25,25 @@ interface PlayerState {
     indexInPlayers?: number;
 }
 
-//set height and width of canvas = window
-let wHeight = window.innerHeight;
-let wWidth = window.innerWidth;
-//canvas element needs to be in a variable
-const canvas = document.querySelector('#the-canvas') as HTMLCanvasElement;
-//context is how we draw! We will be drawing in 2d
-const context = canvas.getContext('2d')!;
-//set the canvas height and width to = window height and width
-canvas.height = wHeight;
-canvas.width = wWidth;
-const player: PlayerState = {} //This will be all things "this" player
-let orbs: any[] = []; //this is a global for all non-player orbs
-let players: any[] = []; //this is an array of all players
+// player info
+const player: PlayerState = {};
 
-//put the modals into variables so we can interact with them
-const loginModal = new bootstrap.Modal(document.querySelector('#loginModal'));
-const spawnModal = new bootstrap.Modal(document.querySelector('#spawnModal'));
+// shared with canvasStuff.ts and socketStuff.ts
+let orbs: any[] = [];
+let players: any[] = [];
 
+// put the modals in variables so we can interact with them
+const loginModal = new bootstrap.Modal(document.querySelector('#loginModal') as HTMLElement); 
+const spawnModal = new bootstrap.Modal(document.querySelector('#spawnModal') as HTMLElement);
 
-window.addEventListener('load',()=>{
-    //on page load, open the login modal
+window.addEventListener('load', () => {
     loginModal.show();
-})
+});
 
-document.querySelector('.name-form').addEventListener('submit',(e)=>{
-    e.preventDefault();
-    // console.log("SUbmitted!")
+document.querySelector('.name-form')?.addEventListener('submit', (event) => {
+    event.preventDefault();
     player.name = (document.querySelector('#name-input') as HTMLInputElement).value;
-    document.querySelector('.player-name')!.innerHTML = player.name!
     loginModal.hide();
     spawnModal.show();
     console.log(player);
-})
-
-document.querySelector('.start-game').addEventListener('click',(e)=>{
-    //hide the start modal
-    spawnModal.hide();
-    //show the hiddenOnStart elements
-    const elArray = Array.from(document.querySelectorAll('.hiddenOnStart'))
-    elArray.forEach(el=>el.removeAttribute('hidden'))
-    init(); //init is iniside of socketStuff.js
-})
+});
