6117b81c1d14e269ac9dcccc7237828577f9ffd6
max.tsai <max.tsai@twsit.com>
2026-09-01 15:15:28 +0800

    51: drawing the player

--- agarClone/public/canvasStuff.ts
+++ agarClone/public/canvasStuff.ts
@@ -4,8 +4,20 @@ const init = () => {
 
 let randomX = Math.floor(500 * Math.random() + 10); // horizontal axis
 let randomY = Math.floor(500 * Math.random() + 10); // vertical axis
+
 context.beginPath();
 context.fillStyle = 'rgb(255, 0, 0)';
+
+// arg1 and arg 2 are center x and y of the arc,
+// arg3 is the radius,
+// arg4 = where to start drawing in radians (0 = 3 o'clock position)
+// arg5 = where to stop drawing in radians (usually Math.PI * 2 for a full circle)
+context.arc(randomX, randomY, 10, 0, Math.PI * 2);
+context.fill();
+context.lineWidth = 3;
+context.strokeStyle = 'rgb(0, 255, 0)';
+context.stroke();
+
 console.log(randomX);
 
 // draw
